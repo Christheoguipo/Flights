d9713da30054e9391e91ce7988f93fff9c7686d3
Christian John Guipo <cjohnguipo@gmail.com>
2023-04-03 19:24:49 +0800

    added country field

--- Flights/Dtos/NewPassengerDto.cs
+++ Flights/Dtos/NewPassengerDto.cs
@@ -17,7 +17,8 @@ namespace Flights.Dtos
         string LastName,
         [Required]
         bool Gender,
-        bool Birthday,
+        DateTime Birthday,
+        string Country,
         string Remarks
         );
 
